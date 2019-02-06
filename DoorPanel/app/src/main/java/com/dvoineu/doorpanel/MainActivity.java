2f8ed473eeac5548d70e512cb35c056546ece074
Aliaksandr Dvoineu <av.dvoinev@gmail.com>
2019-02-06 20:38:08 +0100

    Basic Layout

---
+++ DoorPanel/app/src/main/java/com/dvoineu/doorpanel/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dvoineu.doorpanel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
