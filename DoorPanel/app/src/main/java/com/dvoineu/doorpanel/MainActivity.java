47e3a4e8300a86af89a2813fefa989c097ea9936
Aliaksandr Dvoineu <av.dvoinev@gmail.com>
2019-02-10 17:09:26 +0100

    onClick methods added
    onClick methods to Buttons are added

--- DoorPanel/app/src/main/java/com/dvoineu/doorpanel/MainActivity.java
+++ DoorPanel/app/src/main/java/com/dvoineu/doorpanel/MainActivity.java
@@ -2,8 +2,34 @@ package com.dvoineu.doorpanel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public void onClickUpLeft (View view){
+        Button btnUpLeftNameOfEmployee = (Button) view;
+        Log.i("clicked", "Button clicked!");
+        
+    }
+
+    public void onClickUpRight (View view){
+        Button btnUpRightNameOfEmployee = (Button) view;
+        Log.i("clicked", "Button clicked upRight!");
+    }
+
+    public void onClickBottomRight (View view){
+        Button btnBottomRightNameOfEmployee = (Button) view;
+        Log.i("clicked", "Button clicked upRight!");
+    }
+
+    public void onClickBottomLeft (View view){
+        Button btnBottomLeftNameOfEmployee = (Button) view;
+        Log.i("clicked", "Button clicked left!");
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
