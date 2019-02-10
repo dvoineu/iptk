0b13153d444abbb85340be1b47d10fcdeb776e1e
Aliaksandr Dvoineu <av.dvoinev@gmail.com>
2019-02-10 20:42:10 +0100

    Calendar Activity Added

--- DoorPanel/app/src/main/java/com/dvoineu/doorpanel/CalendarActivity.java
+++ DoorPanel/app/src/main/java/com/dvoineu/doorpanel/CalendarActivity.java
@@ -5,21 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class EmployeeInfoActivity extends AppCompatActivity {
+public class CalendarActivity extends AppCompatActivity {
 
     public void backToMainMenu (View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void toCalendarActivity (View view){
-        Intent intent = new Intent(this, CalendarActivity.class);
-        startActivity(intent);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_employee_info);
+        setContentView(R.layout.activity_calendar);
     }
 }
